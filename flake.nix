975902bb30da0905da83ec31b90fbe00e89772a6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-09-15 09:43:49 +0200

    chore: Add static files.

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "NixOS Processes";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+      let
+        version = self.shortRev or self.lastModifiedDate;
+
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+        packages = [
+          pkgs.plantuml
+        ];
+
+      in {
+        # Nix shell / nix build
+        packages = {
+          default = pkgs.buildEnv {
+            name = "devshell";
+            paths = packages;
+          };
+        };
+
+        # Nix develop
+        devShells = {
+          default = pkgs.mkShellNoCC {
+            name = "devshell";
+            buildInputs = packages;
+          };
+        };
+      }
+    );
+}
